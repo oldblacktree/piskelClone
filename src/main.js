f4ac20701c30612af3f5300bf46d7130863dc6cd
Danil Lukonin <OldBlackTree@gmail.com>
2019-06-15 17:40:49 +0300

    feat: add main section

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
+import { createElementWithClass } from './helpers/helpers';
+
 import Header from './components/header/header';
 import './main.css';
 
+
+const main = createElementWithClass('main', 'main');
 const header = new Header();
+
+
 document.body.appendChild(header.element);
+document.body.appendChild(main);
